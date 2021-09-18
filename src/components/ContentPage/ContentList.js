1b3fb7390ec68aeb468cae7e1393dfa6dcd62974
swasthikshetty10 <swasthikshetty10902@gmail.com>
2021-09-18 20:10:25 +0530

    arranging alphabetically

--- src/components/ContentPage/ContentList.js
+++ src/components/ContentPage/ContentList.js
@@ -9,40 +9,59 @@ import urls from '../../../backend.config'
 function Index() {
     const [dark, setDark] = useContext(DarkLightContext)
     const [ContentData, setContentData] = useState([])
+    const [loading, setLoading] = useState(true)
     const [search, setSearch] = useState("")
     useEffect(() => {
-        axios.get(`${urls.base_url}/article/list?search=${search}`)
-            .then(response => setContentData(response.data));
+        setLoading(true)
+        const timer = setTimeout(() => {
+
+            axios.get(`${urls.base_url}/article/list?search=${search}`)
+                .then(response => {
+                    setContentData(response.data)
+                    console.log(response.data)
+                    setLoading(false)
+                });
+
+        }, 500);
+
+        return () => clearTimeout(timer);
     }, [search]);
     return (
         <section id="content" className="flex-col m-3 shadow-2xl sm:m-5 md:mx-14 border-1  pb-5 border-gray-500 ">
             <Header setSearch={setSearch} />
-            <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                {ContentData.map((ele, index) => {
-                    return <>
-                        <div className={`px-2 border-r-1 border-gray-500 ${Style.headingFillLeft}  `}
-                        >{ele.section ? <h1 className="text-4xl text-semibold">{ele.section}
 
-                        </h1> : <h1 className="h-10"></h1>}
-                            <a href="#" className="flex items-end gap-1">
-                                <h2 className={`text-base font-semibold  cursor-pointer 
-                                            ${dark ? Style.headingFillDark : Style.headingFill} dark-theme`}>
-                                    {ele.headline}
-                                </h2>
-                                <div className=" flex-1 mb-1 border-b-1 border-black dark:border-white border-dashed "></div>
-                                <FiArrowUpRight style={{ fontSize: "1.5rem" }} />
-                            </a>
-                            <div className="lowercase dark:text-adark">
-                                {
-                                    ele.subtopics.map((topic) => {
-                                        return <a href={topic} > {topic},</a>
-                                    })
-                                }
+
+            {loading ? <div>Loading.....</div> : (ContentData.length != 0 ?
+                <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">{
+                    ContentData.map((ele, index) => {
+                        return <>
+                            <div className={`px-2 border-r-1 border-gray-500 ${Style.headingFillLeft}  `}
+                            >{(index > 0) ? (ele.headline.charAt(0).toUpperCase() != ContentData[index - 1].headline.charAt(0).toUpperCase()) ? <h1 className="text-4xl uppercase text-semibold">{ele.headline.charAt(0)}
+
+                            </h1> : <h1 className="h-10"></h1> : <h1 className="text-4xl uppercase text-semibold">{(/[a-zA-Z]/).test(ele.headline.charAt(0)) && ele.headline.charAt(0)}</h1>}
+                                <a href="#" className="flex items-end gap-1">
+                                    <h2 className={`text-base font-semibold  cursor-pointer 
+                                                ${dark ? Style.headingFillDark : Style.headingFill} dark-theme`}>
+                                        {ele.headline}
+                                    </h2>
+                                    <div className=" flex-1 mb-1 border-b-1 border-black dark:border-white border-dashed "></div>
+                                    <FiArrowUpRight style={{ fontSize: "1.5rem" }} />
+                                </a>
+                                <div className="lowercase dark:text-adark">
+                                    {
+                                        ele.subtopics.map((topic) => {
+                                            return <a href={topic} > {topic},</a>
+                                        })
+                                    }
+                                </div>
                             </div>
-                        </div>
-                    </>
-                })}
-            </div>
+                        </>
+                    })
+
+                }</div>
+                : <div className="text-2xl m-5 font-semibold text-center"> No result found </div>)
+            }
+
         </section>
     )
 }
