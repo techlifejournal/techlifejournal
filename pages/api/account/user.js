ccb4f4745c20e331aa2045e62a3651b1db996008
swasthikshetty10 <swasthikshetty10902@gmail.com>
2021-09-25 21:40:44 +0530

    fetch user details

---
+++ pages/api/account/user.js
@@ -0,0 +1,44 @@
+import cookie from 'cookie';
+import urls from '../../../backend.config';
+export default async (req, res) => {
+    if (req.method === 'GET') {
+        const cookies = cookie.parse(req.headers.cookie ?? '');
+        const access = cookies.access ?? false;
+
+        if (access === false) {
+            return res.status(401).json({
+                error: 'User unauthorized to make this request'
+            });
+        }
+
+        try {
+            const apiRes = await fetch(`${urls.base_url}user/`, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Authorization': `Bearer ${access}`
+                }
+            });
+            const data = await apiRes.json();
+
+            if (apiRes.status === 200) {
+                return res.status(200).json({
+                    data: data
+                });
+            } else {
+                return res.status(apiRes.status).json({
+                    error: data.error
+                });
+            }
+        } catch (err) {
+            return res.status(500).json({
+                error: 'Something went wrong when retrieving user'
+            });
+        }
+    } else {
+        res.setHeader('Allow', ['GET']);
+        return res.status(405).json({
+            error: `Method ${req.method} not allowed`
+        });
+    }
+};
