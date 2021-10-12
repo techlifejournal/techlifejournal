0719fc08bc6ba8fe716810ef5c5c74a1eb5d16a3
swasthikshetty <swasthikshetty10902@gmail.com>
2021-10-12 17:38:25 +0530

    loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,24 @@
 import '../styles/globals.css'
 import Contexts from '../src/context'
 import Nav from '../src/components/Navbar'
+import "nprogress/nprogress.css";
+import dynamic from 'next/dynamic'
+const TopProgressBar = dynamic(
+  () => {
+    return import("../src/Utility/TopProgressBar");
+  },
+  { ssr: false },
+);
+
 
 function MyApp({ Component, pageProps }) {
-  return <Contexts><div className={`App`}>
-    <div className="  dark:text-white ">
-      <Nav />
-      <Component {...pageProps} />
-    </div></div>
+  return <Contexts>
+    <TopProgressBar />
+    <div className={`App`}>
+      <div className="  dark:text-white ">
+        <Nav />
+        <Component {...pageProps} />
+      </div></div>
   </Contexts>
 }
 
---
+++ src/Utility/Loader.js
@@ -0,0 +1,11 @@
+
+
+function Loader() {
+    return (
+        <div className="loader">
+
+        </div>
+    )
+}
+
+export default Loader
