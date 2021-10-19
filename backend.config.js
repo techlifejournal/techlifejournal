eb35188cc9c8c278ee48f19a4ed0f9fb3002e09e
swasthikshetty <swasthikshetty10902@gmail.com>
2021-10-19 20:52:35 +0530

    all build errors fixed

--- backend.config.js
+++ backend.config.js
@@ -1,5 +1,9 @@
+
+const client_url = process.env.NODE_ENV === "development" ? "http://localhost:3000" : process.env.URL
+
 const urls = {
     base_url: "https://api-techlifejournal.herokuapp.com/api/",
-    client_url: "http://localhost:3000"
+    client_url,
 }
+
 export default urls
