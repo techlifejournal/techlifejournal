412cb6d14a578983cdc3ef696ebbe51aa7c0ea21
Swasthik Shetty <62538932+swasthikshetty10@users.noreply.github.com>
2021-12-14 21:28:27 +0530

    base url

--- backend.config.js
+++ backend.config.js
@@ -1,7 +1,7 @@
 const url = process.env.URL
-console.log(url)
+const base_url = process.env.LOCALHOST ? "http://127.0.0.1:8000/" : "https://api-techlifejournal.herokuapp.com/api/"
 const urls = {
-    base_url: "https://api-techlifejournal.herokuapp.com/api/",
+    base_url,
     client_url: url,
 }
 export default urls
